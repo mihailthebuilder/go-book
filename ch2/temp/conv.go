172690fbd703105c25c7412a83d7d2e3c3576d5e
Mihail Marian <m.marian@elsevier.com>
2022-02-21 18:49:48 +0000

    finished ex 2.1

--- ch2/temp/conv.go
+++ ch2/temp/conv.go
@@ -9,3 +9,21 @@ func CToF(c Celsius) Fahrenheit {
 func FToC(f Fahrenheit) Celsius {
 	return Celsius((f - 32) * 5 / 9)
 }
+
+func CToK(c Celsius) Kelvin {
+	return Kelvin(c - KelvinToCelsiusDifference)
+}
+
+func KToC(k Kelvin) Celsius {
+	return Celsius(k + KelvinToCelsiusDifference)
+}
+
+func FToK(f Fahrenheit) Kelvin {
+	c := FToC(f)
+	return CToK(c)
+}
+
+func KToF(k Kelvin) Fahrenheit {
+	c := KToC(k)
+	return CToF(c)
+}
